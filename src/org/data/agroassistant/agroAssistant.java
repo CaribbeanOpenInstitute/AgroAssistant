b32b34ed06d47fc728ae8d28c1cd26f22482fc5a
Matthew McNaughton <mamcnaughton@gmail.com>
2011-07-01 12:00:22 -0500

    Icons - Added menu search and reload icons - Added Home screen icons and layout
    Layouts
    - Implemented initial home layout
    - TODO Correctly position buttons and apply colors
    - Added respective strings and layout elements
    
    AgroAssistantJava
    - Added Home method buttons

--- src/org/data/agroassistant/agroAssistant.java
+++ src/org/data/agroassistant/agroAssistant.java
@@ -2,12 +2,32 @@ package org.data.agroassistant;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
 
 public class agroAssistant extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.home);
     }
+    
+    public void onActionOneClick(View v) {
+        Toast.makeText(this, "Action 1", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onActionTwoClick(View v) {
+        Toast.makeText(this, "Action 2", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onActionThreeClick(View v) {
+        Toast.makeText(this, "Action 3", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onActionFourClick(View v) {
+        Toast.makeText(this, "Action 4", Toast.LENGTH_SHORT).show();
+    }
+    
 }
